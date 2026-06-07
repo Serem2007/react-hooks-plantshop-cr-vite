08ef66947595180c9a255e1e6e61c512e4642dda
Levy Serem <levy.serem@student.moringaschool.com>
2026-06-07 09:20:32 -0700

    Complete Plantshop lab

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  function addPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={displayedPlants} />
     </main>
   );
 }
